7eb68934ef7d7aba0f20c885c502916c056b187d
avnergomes <avnerpaesgomes@gmail.com>
2025-11-12 07:06:34 -0300

    update

--- utils/loaders.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(path):
-    if path.endswith(".xlsx"):
-        return pd.read_excel(path)
-    elif path.endswith(".csv"):
-        return pd.read_csv(path, sep=";")
-    else:
-        return pd.DataFrame()
